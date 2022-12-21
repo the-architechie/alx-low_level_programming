5fd5a44e1aaa882f60ad18f55f53a4e282cf8ab2
the-architechie <getmuchaminow@gmail.com>
2022-12-21 02:52:14 +0000

    fix betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,13 @@
 
 /**
  * reset_to_98 - Takes a pointer to an int and updates is point to 98
- * n: the pointer to an integer
+ * @n: the pointer to an integer
  *
  * Return: None
  */
 
 void reset_to_98(int *n)
 {
+
   *n = 98;
 }
