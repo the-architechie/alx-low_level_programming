49c78a1c48d50917126d4eeec3e8791f4e5de9e0
the-architechie <getmuchaminow@gmail.com>
2023-01-02 20:07:22 +0000

    Change while loop with for loop

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while ((*(dest + i) = *(src + i)) != '\0' && i < n)
-	{
-		i++;
-	}
-	while(i <= n)
-	{
-		*(dest + i) = '\0';
-		i++;
-	}
 
-	return (dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
+
 }
