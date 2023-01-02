2c7b4772d10899de78ed69c53593d0a817b0c5dd
the-architechie <getmuchaminow@gmail.com>
2023-01-02 10:20:56 +0000

    Fix error

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,7 +22,7 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 	j = 0;
-	while ((*(dest + len) = *(src + j)) && len < n)
+	while ((*(dest + len) = *(src + j)) && j <= n - 1)
 	{
 		len++;
 		j++;
