6fc0379b116977b359f2b6bdba7dec358e710eea
the-architechie <getmuchaminow@gmail.com>
2023-01-03 12:21:14 +0000

    remove the n derement

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -16,8 +16,7 @@ void reverse_array(int *a, int n)
 	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
-		a[i] = a[n - 1];
-		a[n - 1] = temp;
-		n--;
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
 	}
 }
