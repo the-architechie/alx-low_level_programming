61c08686e81784d6e159d37cd88f6037d43c1470
the-architechie <getmuchaminow@gmail.com>
2023-01-03 12:04:41 +0000

    Add a function that reverses elements of an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * reverse_array - reverses the content of array of integers
+ * @a: the pointer to an array of integers
+ * @n: the number of elements of the array
+ * Return: Returns Nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = temp;
+		n--;
+	}
+	printf("first digit is %d\n", a[i]);
+}
