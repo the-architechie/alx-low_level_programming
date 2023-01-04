ee89e5d7826f967dd5592e3f30eac3c0e9e8c3ba
the-architechie <getmuchaminow@gmail.com>
2023-01-04 12:46:52 +0000

    add 7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: the string to be encoded
+ * Return: Returns the resulting string
+ */
+
+char *leet(char *str)
+{
+	int i;
+	int j = 0;
+
+	char c[] = {'a', 'e', 'o', 'i', 't', 'l'};
+	char nums[] = {4, 3, 0, 7, 1};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == c[j] || str[i] == c[j] - 32)
+			{
+				str[i] = nums[j] + '0';
+			}
+		}
+	}
+	return (str);
+}
